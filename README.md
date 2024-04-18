0de6d491cedafb1464f3e90a34b23dbdc75da8f6
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-04-17 17:53:42 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h2>Word Counter project </h2>
+
+<br/> It's a simple Word Counter Project using TypeScript. This handy tool first prompts users to select whether they'd like to count words or letters. Once chosen, it efficiently calculates either the word count or letter count accordingly. Users simply input a sentence, and voila! The word counter displays the desired count.
+<br/> Additionally, I've incorporated sleek chalk-style design for a visually pleasing experience..
+<br/><br/>You can also Experience my this Word Counter project firsthand in your command line interface by running <br>"npx fiza07_word_counter"<br/>
---
+++ index.ts
@@ -0,0 +1,68 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+import chalk from "chalk"
+import clear from "clear"
+
+async function animate(text:string) {
+    for(let char of text){
+        process.stdout.write(char);
+        await new Promise((resolve) => {
+           setTimeout(resolve,40) 
+        })
+    }
+}
+   clear()
+   await animate (chalk.greenBright.bold.underline("\n\t\tWELCOME TO THE FIZA'S WORD COUNTER PROJECT\n"))
+   await animate (chalk.grey("\n\t\t-------------------------------------------\n\n"))
+
+async function main() {
+
+let options = await inquirer.prompt({
+    name : "option" ,
+    type : "list" ,
+    message :chalk.bgGray.blue("What's you want to count\n"),
+    choices : ["Words count","Letters count"]
+})
+
+if (options.option === "Words count"){
+let answer1 = await inquirer.prompt({
+    name : "Sentence" ,
+    type : "input" ,
+    message : chalk.yellowBright("Enter Your sentence to count the word : \n")
+})
+// for  counting words in sentence
+
+const sentence = answer1.Sentence.trim().split(" ")
+console.log(sentence)
+await animate(chalk.yellow.underline(`\n\t\tYour Words in Sentence : ${sentence.length}\n`));
+
+}else{
+let answer2 = await inquirer.prompt({
+    name : "Letter" ,
+    type : "input" ,
+    message : chalk.yellowBright("Enter Your sentence to count the letters : \n")
+})
+
+// for  counting letters in sentence
+
+const letter = answer2.Letter.replace(/\s/g, "");
+await animate (chalk.yellow.underline(`\n\t\tYour Letters in Sentence : ${letter.length}\n`));
+}
+
+  await animate (chalk.grey("\n\t------------------------------------------\n\n"))
+
+let restart = await inquirer.prompt({
+    name: "restart",
+    type : "confirm" ,
+    message : chalk.red("Do you want to exit ?\n")
+})
+if(restart.restart){
+    await animate(chalk.magenta.underline("\n\t\tThanks for using my Word Counter project :-) \n"))
+    await animate (chalk.grey("\n\t\t-------------------------------------------\n\n"))
+  }
+    else {
+        clear();
+        main();
+    }
+}
+main()
